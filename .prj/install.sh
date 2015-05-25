fb69ef625e2b66b9b3af0f0ac0542416f1342dee
Ninguém <spamgithub@rasparta.org>
2015-05-25 17:35:47 +0100

    Install script.
    The prj directory, as cloned by git, is now a propper prj project.
    It has a .prj directory with an install.sh script that installs prj to the first writable directory mentioned in your PATH variable.

---
+++ .prj/install.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+## CONFIGURATIONS:
+#
+#  Assign the name of the script to be installed to $originalscriptname.
+originalscriptname=$(basename $(find $(dirname $0)/.. -maxdepth 1 -type f -executable -readable))
+#
+
+#  The name of this script.
+scriptname=$(basename $0 .sh)
+#
+
+#  The function of this script
+scriptfunction="Installs $originalscriptname to the first writable directory in your \$PATH."
+#
+
+#  If everything is configured, set the $configured variable to "Yes".
+configured=Yes
+#
+## CONFIGURATION END
+
+## AUTO-INICIALIZATION
+#
+#  function aboutme (don't change!)
+aboutme() {
+echo "$scriptfunction"
+}
+
+#  What to do if not configured?
+notconfigured() {
+    cat << EOF
+Configure me first. It'll only take you a moment.
+ 
+EOF
+    exit 1
+}
+
+#  What to do if in panic?
+showhelp() {
+    cat << EOF
+Name:
+	$scriptname - $scriptfunction
+ 
+Usage:
+	[$originalscriptname run $originalscriptname] $scriptname [aboutme|help]
+	[$originalscriptname run $originalscriptname] $scriptname
+ 
+EOF
+}
+#
+
+#  Am I configured?
+[ "$configured" != "Yes" ] && notconfigured
+#
+## CONFIGURATION end
+#  Ok, let's go then...
+
+case $1 in
+
+    aboutme)
+	aboutme
+	exit 0
+	;;
+
+    help)
+	showhelp
+	exit 0
+	;;
+
+    *)
+	OLD_IFS=$IFS
+	IFS=':'
+	for dir in $PATH
+	do
+	    if [ -w $dir ]
+	    then
+		cp -i $(dirname $0)/../$originalscriptname $dir && exit 0
+	    fi
+	done
+	pathdirwritable="No"
+	for dir in $PATH
+	do
+	    if [ -w $dir ]
+	    then
+		pathdirwritable="Yes"
+	    fi
+	done
+	if [ $pathdirwritable = "No" ]
+	then
+	    echo "No writable directory in your \$PATH!"
+	else
+	    echo "Something wrong must have happened!"
+	fi
+	exit 1
+	;;
+esac
+
+exit 1
